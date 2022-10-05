3d883deb42a85c3d75b7189a768362d64aa8a6ee
winning godspower <winninggodspower@gmail.com>
2022-10-05 14:16:38 +0100

    added member model to admin panel

--- user_authentication/admin.py
+++ user_authentication/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # importing the use model
+from .models import Member
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Member)
